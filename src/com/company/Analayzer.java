ccfc700bbe24a1093a7eab75db0bddde5fc2e1f2
VinniPxxh <kuznetsove2001@mail.ru>
2022-04-04 18:29:42 +0300

    New Analyse

--- src/com/company/Analayzer.java
+++ src/com/company/Analayzer.java
@@ -7,12 +7,11 @@ public class Analayzer {
         }
         return Label.OK;
     }
-    public class SpamAnalyzer extends KeywordAnalyzer implements TextAnalyzer {
-        private String[] keywords;
+    public class SpamAnalyzer extends KeywordAnalyzer {
+        public String[] keywords;
 
-        SpamAnalyzer(String[] keywords) {
+        public SpamAnalyzer(String[] keywords) {
             this.keywords = keywords;
-
         }
 
         @Override
@@ -24,11 +23,10 @@ public class Analayzer {
         protected Label getLabel() {
             return Label.SPAM;
         }
-
     }
 
-    public abstract class  NegativeTextAnalyzer extends KeywordAnalyzer {
-        public String[] keywords_negative = {":(", ":|", "=("};
+    public class NegativeTextAnalyzer extends KeywordAnalyzer {
+        private String[] keywords_negative = {":(", ":|", "=("};
 
         @Override
         protected String[] getKeywords() {
@@ -40,31 +38,30 @@ public class Analayzer {
             return Label.NEGATIVE_TEXT;
         }
     }
-    public class TooLongTextAnalyzer implements TextAnalyzer {
 
+    public class TooLongTextAnalyzer implements TextAnalyzer {
         private int maxLength;
 
-        public TooLongTextAnalyzer( int maxLength) {
+        TooLongTextAnalyzer(int maxLength) {
             this.maxLength = maxLength;
         }
 
         @Override
         public Label processText(String text) {
-            if(text.length() > maxLength) return Label.TOO_LONG;
-
+            if (text.length() > maxLength) return Label.TOO_LONG;
             return Label.OK;
         }
     }
 
     public abstract class KeywordAnalyzer implements TextAnalyzer {
-
-        abstract String[] getKeywords();
+        protected abstract String[] getKeywords();
 
         protected abstract Label getLabel();
 
         @Override
         public Label processText(String text) {
-            text : getKeywords();
+            text:
+            getKeywords();
             if (text.contains(text)) return getLabel();
 
             return Label.OK;
